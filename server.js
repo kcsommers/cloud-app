3fd7df72e6205d90a3f490c8a674c7caa1798f3d
Mark Sommers <kacysommers@gmail.com>
2018-06-13 15:58:04 -0700

    added images to cloudinary

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
+require('dotenv').config();
 var express = require('express')
 var path = require('path');
 var ejsLayouts = require('express-ejs-layouts');
-require('dotenv').config();
+var multer = require('multer');
+var upload = multer({dest: './uploads/'});
+var cloudinary = require('cloudinary');
 var app = express();
 var port = process.env.PORT || 2000;
 
 app.set('view engine', 'ejs');
 app.use(ejsLayouts);
 
+var images = [];
+
 app.get('/', function(req, res) {
 	console.log('get route hit');
-	res.render('index');
+	res.render('index', {arr: images, cloudinary: cloudinary});
 });	
 
 app.get('/upload', function(req, res) {
@@ -18,9 +23,12 @@ app.get('/upload', function(req, res) {
 	res.render('upload');
 });
 
-app.post('/upload', function(req, res) {
+app.post('/upload', upload.single('myFile'), function(req, res) {
 	console.log('POST - upload hit');
-	res.send('You done good');
+	cloudinary.uploader.upload(req.file.path, function(result) {
+		images.push(result.public_id);
+		res.redirect('/');
+	});
 });
 
 app.listen(port, function() {
